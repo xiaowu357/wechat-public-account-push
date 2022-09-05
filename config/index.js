2af3ec95ab2217cf62b5c691d75b67b9f069fa10
xiaowu357 <112889899+xiaowu357@users.noreply.github.com>
2022-09-05 23:52:09 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -20,7 +20,7 @@ export const config = {
    *    // 想要发送的人的名字
    *    name: "宝贝",
    *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-   *    id: "orcNj6GiqpZYYqRz8t_ErCWJdPiU",
+   *    id: "orcNj6PaCLWYhhkv5cksvh3eC4fU",
    *    // 你想对他发送的模板消息的模板ID
    *    useTemplateId: "4ttkKuEnCL_ftbQ319BVqTYuiipMoMoJyfFpvcTdDr0",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
@@ -33,9 +33,9 @@ export const config = {
       // 想要发送的人的名字
       name: "宝贝",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohxOO6VskY2v40Kuf5ywIZVRM3puXhk",
+      id: "orcNj6PaCLWYhhkv5cksvh3eC4fU",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "Wue80GrtY1nAi5a4tam4FDxZF59WNx9pwcBc06LMpANnu8",
+      useTemplateId: "4ttkKuEnCL_ftbQ319BVqTYuiipMoMoJyfFpvcTdDr0",
       // 所在省份
       province: "山东省",
       // 所在城市
